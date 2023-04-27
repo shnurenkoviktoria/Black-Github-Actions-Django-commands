1c44bf1551ba7806b19ccc9e4027c25207398cb3
osm1nozhek <snurenkoviktoria@gmail.com>
2023-04-27 22:26:02 +0300

    Force all code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from .models import Teacher
 
 
-
 def teachers_list(request):
     teachers = list(Teacher.objects.values())
-    return render(request, "teachers_list.html", context={'teachers': teachers})
+    return render(request, "teachers_list.html", context={"teachers": teachers})
